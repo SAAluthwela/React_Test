bcd8829b49187cb625d19a51de6e62466185e4b5
SAAluthwela <58663644+SAAluthwela@users.noreply.github.com>
2021-10-10 17:36:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+const api = {
+  key: "7d4ef5d23648c7c62acd3ccfa1a0a30b",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+function App() {
+ 
+}
+
+export default App;
